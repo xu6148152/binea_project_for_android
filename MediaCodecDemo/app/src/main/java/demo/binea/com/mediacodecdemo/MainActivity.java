c35334eed290898522db9b4c3d841fb964f77eea
xubinggui <xubinggui@zepplabs.com>
2015-07-08 16:23:31 +0800

    add MediaCodecDemo

---
+++ MediaCodecDemo/app/src/main/java/demo/binea/com/mediacodecdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package demo.binea.com.mediacodecdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.texture_view);
+    }
+}
