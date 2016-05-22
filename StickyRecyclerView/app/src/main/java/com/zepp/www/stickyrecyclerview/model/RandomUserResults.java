7fca53988672414263ae4aeada730aeb7f9f82b5
xu6148152 <xu6148152@gmail.com>
2016-05-23 00:30:07 +0800

    Add Demo

---
+++ StickyRecyclerView/app/src/main/java/com/zepp/www/stickyrecyclerview/model/RandomUserResults.java
@@ -0,0 +1,38 @@
+package com.zepp.www.stickyrecyclerview.model;
+
+/**
+ * Created by xubinggui on 5/23/16.
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ */
+public class RandomUserResults {
+    public static class Info {
+        String seed;
+        int results;
+        int page;
+        String version;
+    }
+
+    public Person[] results;
+    public Info info;
+    public String error;
+}
